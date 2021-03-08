eb8ace150016ad7056014ef2aa2f5535939e2c51
soner <s.ism.candan@gmail.com>
2021-03-08 13:14:52 +0000

    minor: tidying up

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,13 @@
 import pytest
-assert(1==2)
+import flask
+from flask import request, jsonify
+from flask import Flask, Response as BaseResponse, json
+from flask.testing import FlaskClient
+from .. import app
+print(app)
+# flask app routes
+def test_home():
+    with app.test_request_context('/') :
+        assert(flask.request.path == '/')
+        assert(flask.request.status_code==200)
+   
