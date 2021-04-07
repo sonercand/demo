5faec5c7a3d3b66907536bf75ea865b968f1a704
Soner Candan <soner@soners-mbp.home>
2021-04-07 14:02:33 +0100

    test predict

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,5 +11,24 @@ def test_home():
 
 def test_predict():
     with app.test_client() as c:
-      r = c.post('/predict')
+      r = c.post('/predict',payload={
+                "CHAS":{
+                    "0":0
+                },
+                "RM":{
+                    "0":6.575
+                },
+                "TAX":{
+                    "0":296.0
+                },
+                "PTRATIO":{
+                    "0":15.3
+                },
+                "B":{
+                    "0":396.9
+                },
+                "LSTAT":{
+                    "0":4.98
+                }
+                })
       assert(r.status_code == 200)  
